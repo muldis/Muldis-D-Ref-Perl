023fe3b56d70a87235d1b8c7632cbf98c288dab6
Darren Duncan <darren@DarrenDuncan.net>
2011-12-26 23:21:26 -0800

    first commit - .gitignore, starter README, starter lib/Muldis/D/RefEng.pm

---
+++ lib/Muldis/D/RefEng.pm
@@ -0,0 +1,18 @@
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Muldis::D::RefEng -
+Reference Implementation of Muldis D Over Perl 5
+
+=head1 AUTHOR
+
+Darren Duncan - darren@DarrenDuncan.net
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright © 2011, Muldis Data Systems, Inc.
+
+=cut
